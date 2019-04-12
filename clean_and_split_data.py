ec64f8a69633bf24f01ecc344effa1ac8ecca7e9
Ankur Gupta <rukna1000@gmail.com>
2019-04-12 15:39:45 -0400

    Clean trump data

--- clean_and_split_data.py
+++ clean_and_split_data.py
@@ -1,11 +1,17 @@
 import pandas as pd
 import numpy as np
+import string
+import re
+
+TRAIN_RATIO = .7
 
 def clean_tweet_text(tweet):
+    tweet = re.sub(r'\\\'9[0-9]', '', tweet)
+    tweet = tweet.translate(str.maketrans('', '', string.punctuation))
     words = tweet.split(' ')
 
     for index, word in enumerate(words):
-        if 'https://' in word:
+        if 'https' in word:
             del words[index]
 
     return ' '.join(words)
@@ -13,5 +19,6 @@ def clean_tweet_text(tweet):
 if __name__ == '__main__':
     data_raw = pd.read_csv("TrumpTweets.csv")
     no_rts = data_raw[data_raw['is_retweet'] == 'false']
-
     no_rts['text'] = no_rts['text'].apply(clean_tweet_text)
+
+    no_rts.to_csv("trump_clean.csv")
