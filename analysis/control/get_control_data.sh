e2decfcb2fed6455a17c2361d2dbb700d6210fcd
Ankur Gupta <rukna1000@gmail.com>
2019-04-28 09:41:45 -0400

    keep GOT clean

--- analysis/control/get_control_data.sh
+++ analysis/control/get_control_data.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-docker run -it -v `pwd`/GOT:/proj -v `pwd`/get_control.py:/proj/get_control.py python:2 bash
+cp get_control.py GOT/
+
+docker run -it -v `pwd`/GOT:/proj -v `pwd`/get_control.py:/proj/get_control.py ds3001:got
+
+mv GOT/control_tweets.csv .
+rm GOT/get_control.py 
