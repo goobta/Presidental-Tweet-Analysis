873af62781abfba393a8ecc8fe96fe8b2dbe8e24
Ankur Gupta <rukna1000@gmail.com>
2019-04-28 09:57:33 -0400

    Update mv permission

--- analysis/control/get_control_data.sh
+++ analysis/control/get_control_data.sh
@@ -4,5 +4,5 @@ cp get_control.py GOT/
 
 docker run -it -v `pwd`/GOT:/proj -v `pwd`/get_control.py:/proj/get_control.py ds3001:got
 
-mv GOT/control_tweets.csv .
+mv -f GOT/control_tweets.csv .
 rm GOT/get_control.py 
